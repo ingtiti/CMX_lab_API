70a424b50424bc993de43bb750eb87c5dbf28288
ingtiti <ing.titi@gmail.com>
2016-10-19 15:34:46 -0500

    Update to include the main titles
    Edit original code to show main titles and use same API as CLAU system
    training

--- API_connect.py
+++ API_connect.py
@@ -6,35 +6,35 @@ import base64
 
 
 def main():
-   print("*********************************************************");
-   print("* Cisco CMX Command Line REST API Python Utility        *");
-   print("* Please provide the input in the following format      *");
-   print("*                                                       *");
-   print("* REST URL: https://[ip]/api/location/v1/clients/count  *");
-   print("* Username: [username]                                  *");
-   print("* Password: [password]                                  *");
-   print("*                                                       *");
-   print("*                                                       *");
-   print("* Control C to exit                                     *");
-   print("*********************************************************");
+   print("************************************************************");
+   print("* Cisco CMX Command Line REST API Python Utility           *");
+   print("* Please provide the input in the following format         *");
+   print("*                                                          *");
+   print("* REST URL: https://[ip]/api/analytics/v1/now/clientCount  *");
+   print("* Username: [username]                                     *");
+   print("* Password: [password]                                     *");
+   print("*                                                          *");
+   print("*                                                          *");
+   print("* Control C to exit                                        *");
+   print("************************************************************");
 
-   storedCredentials = False
+   try:
+       username = 'learning'
+       password = 'learning'
+       print(" ")
+       print ("user y password asignados")
+       restURL = 'https://msesandbox.cisco.com:8081/api/analytics/v1/now/clientCount'
+       request = requests.get(
+       url = restURL,
+       auth = HTTPBasicAuth(username,password),
+       verify=False)
+       print(" ")
+       print ("GET enviado")
+       parsed = json.loads(request.content)
+       print (json.dumps(parsed, indent=2))
 
+   except requests.exceptions.RequestException as e:
+               print(e)
 
-try:
-
-   username = 'learning'
-   password = 'learning'
-   print ("user y password asignados")
-   restURL = 'https://msesandbox.cisco.com:8081/api/location/v1/clients/count'
-   request = requests.get(
-   url = restURL,
-   auth = HTTPBasicAuth(username,password),
-   verify=False)
-   print ("GET enviado")
-   parsed = json.loads(request.content)
-   print (json.dumps(parsed, indent=2))
-
-except requests.exceptions.RequestException as e:
-           print(e)
-           print(e)
+if __name__ == "__main__":
+    main()
